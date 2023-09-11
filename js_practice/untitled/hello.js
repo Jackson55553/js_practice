9e3f0def04399ba116c140d98fc394f932928175
Evgeny Dudin <Dudin10@mail.ru>
2023-09-11 19:44:37 +0500

    4:47:40

--- js_practice/untitled/hello.js
+++ js_practice/untitled/hello.js
@@ -1,3 +1,13 @@
 console.log("hello");
-let number = 3+2;
+let number = 3 + 2;
 console.log(number);
+const me = {
+    name: "jacson",
+    work: true,
+    id: 22345
+}
+
+const me2 = {...me }
+
+console.log(!me)
+
